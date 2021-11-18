547b49f66e66e81d0e6135bb086ba02f40fdab5d
Andzej-F <70884391+Andzej-F@users.noreply.github.com>
2021-11-18 22:17:49 +0200

    .

--- main.php
+++ main.php
@@ -1,195 +1,97 @@
 
 <?php
 
-// $wordsArray = file('https://gist.githubusercontent.com/cosmologicon/1e7291714094d71a0e25678316141586/raw/006f7e9093dc7ad72b12ff9f1da649822e56d39d/tex-hyphenation-patterns.txt');
+echo hyphenate('mistranslate') . '<br>';
+echo hyphenate('vigorous') . '<br>';
+echo hyphenate('changed') . '<br>';
+echo hyphenate('pitch') . '<br>';
+echo hyphenate('uncopyrightable') . '<br>';
+echo hyphenate('system') . '<br>';
+echo hyphenate('disastrous') . '<br>';
+echo hyphenate('frightening') . '<br>';
+echo hyphenate('encouraging') . '<br>';
 
-// $array = [
-//     0 => "a2n",
-//     1 => ".mis1",
-//     2 => "m2is",
-//     3 => "2n1s2",
-//     4 => "n2sl",
-//     5 => "s1l2",
-//     6 => "s3lat",
-//     7 => "st4r",
-//     8 => "4te.",
-//     9 => "1tra",
-// ];
-
-$start_array = [0 => ".mis1"];
-$end_array = [0 => "4te."];
-$middle_array = [
-    0 => "a2n",
-    1 => "m2is",
-    2 => "2n1s2",
-    3 => "n2sl",
-    4 => "s1l2",
-    5 => "s3lat",
-    6 => "st4r",
-    7 => "1tra"
-];
-
-$input = "mistranslate";
-
-echo hyphenate('mistranslate', [0 => '.mis1']);
-
-function hyphenate($input, $array)
+function hyphenate($input)
 {
+    $array = getMatch($input);
+
+    $symbol = '*';
+
+    $input_format = "." . addSymbol($input, $symbol) . "."; // .m*i*s*t*r*a*n*s*l*a*t*e.
+
     foreach ($array as $phrase) {
 
-        $input_ast = addAsterisk($input);
-        echo $input_ast . '<br>';
-        $phrase_ast = addAsterisk($phrase);
-        echo $phrase_ast . '<br>';
+        $phrase_ast = addSymbol($phrase, $symbol); // .m*i*s1        
 
-        //Check if phrase matches the input
-        if (str_contains($input_ast, $phrase_ast)) {
-            // Check if  begins with dot
-            if (str_starts_with($phrase, '.')) {
+        $phrase_regex = preg_replace('/[\d,\*]/', '[\*,\d]', $phrase_ast); // /[\*,\d]n[\*,\d]s[\*,\d]/
 
-                // Check if the input begins with the given phrase
-                if (str_starts_with($input_ast, $phrase_ast)) {
+        if (preg_match("/$phrase_regex/", $input_format, $output_array)) {
 
-                    $start_pos = strpos($input_ast, $phrase_ast);
-                    echo $start_pos;
+            $start_pos = strpos($input_format, $output_array[0]); // 0
 
-                    // Get the positions of numbers in phrase
-                    $nbr_array = numberPosition($phrase);
+            // Get the positions of numbers in the phrase
+            $nbr_array = numberPosition($phrase_ast); // [6 => 1]
 
-                    // mis1
-                    // 0123
-                    // m*i*s*
-                    // 012345
-                    foreach ($nbr_array as $position => $value) {
-                        $pos = $start_pos + $position;
+            foreach ($nbr_array as $position => $value) {
+                $pos = $start_pos + $position; // 6
 
-                        // Chek if the given input position already has a number
-                        if (ctype_digit($input_ast[$pos])) {
+                // Chek if the given input position already has a number
+                if (ctype_digit($input_format[$pos])) {
 
-                            // Chek if it is bigger than the number in a phrase
-                            echo "<br>kuku " . $input_ast[$pos];
-                            $max_value = max($input_ast[$pos], $value);
+                    // Chek if it is bigger than the number in a phrase
+                    $max_value = max($input_format[$pos], $value);
 
-                            $input_ast = substr_replace($input_ast, $max_value, $pos, 1);
-                        }
-                        if ($input_ast[$pos] === '*') {
-                            echo "<br>kuku " . $input_ast[$pos];
-                            echo "<br>kuku " . $value;
-                            echo "<br>kuku " . $pos;
-                            $input_ast = substr_replace($input_ast, $value, $pos, 1);
-                        }
-                    }
+                    $input_format = substr_replace($input_format, $max_value, $pos, 1);
                 }
+                if ($input_format[$pos] === '*') {
+
+                    $input_format = substr_replace($input_format, $value, $pos, 1);
+                }
+            }
+        }
+    }
+
+    // remove dots, asterisks, even numbers
+    $input_format = preg_replace('/[\.,\*,2,4,6,8,0]/', '', $input_format);
+
+    // Replace odd numbers with hyphens
+    $input_format = preg_replace('/[1,3,5,7,9]/', '-', $input_format);
+
+    return $input_format;
+}
+
+function getMatch($input_org)
+{
+    $array = file('https://gist.githubusercontent.com/cosmologicon/1e7291714094d71a0e25678316141586/raw/006f7e9093dc7ad72b12ff9f1da649822e56d39d/tex-hyphenation-patterns.txt', FILE_IGNORE_NEW_LINES);
+    $matches = [];
+
+    $input = ".$input_org."; // .mistranslate
+
+    foreach ($array as $key => $phrase) {
+        $phrase_format = preg_replace('/\d/', '', $phrase);
+
+        if (str_starts_with($phrase_format, '.')) {
+            $regex = "/^$phrase_format/";
+
+            if (str_starts_with($input, $regex)) { // $input = .mistranslate.;  $regex= '/^.mis/'
+                $matches[$key] = $phrase;
+            }
+        } elseif (str_ends_with($phrase_format, '.')) {
+            $regex = "/$phrase_format$/";
+
+            if (str_ends_with($input, $regex)) {
+                $matches[$key] = $phrase;
             }
         } else {
-            echo "No match found<br>";
+            $regex = "/$phrase_format/";
+        }
+
+        if (preg_match($regex, $input)) {
+            $matches[$key] = $phrase;
         }
     }
-    echo '<pre>';
-    print_r(get_defined_vars());
-    echo '</pre>';
-    return $input_ast;
+    return !empty($matches) ? $matches : $array;
 }
-/*
-        //            "2n1s2"
-        //            "*n*s*"
-        //  m*i*s*t*r*a*n*s*l*a*t*e
-
-        //        "s1l2"
-        //  mistrans1l2ate
-
-        //        "s3lat"
-        //  mistrans3l2ate
-
-        // add asterisks
-        // mis1translate => m*i*s1t*r*a*n*s*l*a*t*e
-
-        //1 check if the phrase begins with dot
-        // str_ends_with($string $haystack, string $needle):bool;
-
-        // 2 check if the phrase ends with dot
-        // if yes, remove the dot from phrases end
-        // check if it matches 
-        // if yes, check if it contains any numbers
-        // add the phrase to the input's end
-
-        // 3 for the rest phrases
-        // check if it matches 
-        // if yes, check if it contains any numbers
-        // add the phrase to the input's end
-
-        // Remove the numbers from the phrases
-        // debug: $phrase = ".mis1";
-        $phrase_trim = preg_replace('/[\d]/', '', $phrase);
-        // debug: $phrase_trim='.mis';
-
-        // Remove the numbers and asterisks from the input
-        // debug: $input = 'mistranslate';
-        $input_trim = preg_replace('/[\d,\*]/', '', $input);
-        // debug: $input_trim='mistranslate';
-
-        if (preg_match("/$phrase_trim/", $input_trim)) {
-
-            // Positions of numbers in a phrase $position => $value
-            $positions = [];
-
-            // Split given phrase into an array
-            $phrase_array = str_split($phrase, 1);
-
-            foreach ($phrase_array as $position => $value) {
-                if (is_numeric($value)) {
-                    // Save matched number positions in the array
-                    $positions += [$position => $value]; // 0=>2, 1=>2, 2=>4
-                }
-            }
-
-            $phrase = addAsterisk($phrase);
-
-            // Additionally for phrase change the numbers into asterisks, for matching purposes
-            $phrase = preg_replace('/\d/', '*', $phrase); // 2n1s2 \*n\*s\*
-
-            $input = addAsterisk($input);
-            // echo "line " . __LINE__ . " input " . $input . '<br>';
-
-            $start_pos = strpos($input, $phrase);
-
-            if ($start_pos !== false) {
-
-                foreach ($positions as $position => $value) {
-
-                    $pos = $start_pos + $position;
-
-                    // echo "line " . __LINE__ . " input[pos]" . $input[$pos] . '<br>';
-                    // Chek if the given input position already has a number
-                    if (ctype_digit($input[$pos])) {
-                        // echo substr_replace('m*i*s*t', '5', 3, 1);// m*i5s*t
-
-                        // echo "line " . __LINE__ . " input[pos]" . $input[$pos] . '<br>';
-                        // Chek if it is bigger than the number in a phrase
-                        $input[$pos] = max($input[$pos], $value);
-                        $input = substr_replace($input, $value, $pos, 1); // m*i5s*t
-                        // echo "line " . __LINE__ . " input " . $input . '<br>';
-                    }
-                    if ($input[$pos] === '*') {
-                        // echo "line " . __LINE__ . " input[pos]" . $input[$pos] . '<br>';
-                        // $input[$pos] = $value;
-                        // echo "<hr>line " . __LINE__ . " input " . $input . '<br>';
-                        // echo "line " . __LINE__ . " pos " . $pos . '<br>';
-                        // echo "line " . __LINE__ . " value " . $value . '<br>';
-                        $input = substr_replace($input, $value, $pos, 1); // m*i5s*t
-
-                        // echo "line " . __LINE__ . " input" . $input . '<br>';
-                    }
-                }
-            }
-        }
-    }
-    // echo "<pre>";
-    // print_r(get_defined_vars());
-    // echo "</pre>";
-    return $input;
-}
-*/
 
 /**
  * Function finds positions of numbers in a string
@@ -218,9 +120,14 @@ function numberPosition(string $phrase): array
 }
 
 /**
- * Function adds asterisks between two adjacent letters 
+ * Function adds symbols between two adjacent letters 
+ * 
+ * @param string $word Input word
+ * @param string $aymbol Input word
+ * 
+ * @return string
  * */
-function addAsterisk($word)
+function addSymbol($word, $symbol)
 {
     // Convert input string to array
     $array = str_split($word, 1);
@@ -228,54 +135,9 @@ function addAsterisk($word)
     // Add asterisks between two adjacent letters in input
     for ($i = 0; $i < (count($array) - 1); $i += 1) {
         if (ctype_alpha($array[$i]) && ctype_alpha($array[$i + 1])) {
-            $array[$i] .= '*';
+            $array[$i] .= $symbol;
         }
     }
 
-    $word = implode('', $array);
-
-    if (preg_match('/[\d,\.]/', $word)) {
-
-        // return preg_replace('/[\d,\.]/', '*', $word);
-        return $word;
-    }
-
-    return "*$word*";
+    return implode('', $array);
 }
-// substr_replace(
-//     array|string $string,
-//     array|string $replace,
-//     array|int $offset,
-//     array|int|null $length = null
-// ): string|array
-
-// echo '<br><br>';
-// echo substr_replace('m*i*s*t', '5', 3, 1);// m*i5s*t
-// $input = '.m*i*s*t*r*a*n*s*l*a*t*e.';
-// $pos = 12;
-// $value = 2;
-// $input = substr_replace($input, $value, $pos, 1); // m*i5s*t
-// echo '<br>****************************************************************<br>';
-// echo $input;
-// echo max(7, "3"); // 7
-
-//str_starts_with(string $haystack, string $needle): bool
-// if (str_starts_with('abc', 'ab')) {
-//     echo "True, there is a match<br>";
-// } else {
-//     echo "No match found<br>";
-// }
-// $middle_array = [
-//     0 => "a2n",
-//     1 => "m2is",
-//     2 => "2n1s2",
-//     3 => "n2sl",
-//     4 => "s1l2",
-//     5 => "s3lat",
-//     6 => "st4r",
-//     7 => "1tra"
-// ];
-
-// foreach ($middle_array as $phrase) {
-//     echo $phrase . '<br>';
-// }
--- tests/MainTest.php
+++ tests/MainTest.php
@@ -7,21 +7,22 @@ use PHPUnit\Framework\TestCase;
 class MainTest extends TestCase
 {
 
-    function testAddAsterisk()
+    function testAddSymbol()
     {
         require 'main.php';
 
-        $this->assertEquals('*m*i*s*t*r*a*n*s*l*a*t*e*', addAsterisk('mistranslate'));
-        $this->assertEquals('a2n', addAsterisk('a2n'));
-        $this->assertEquals('.m*i*s1', addAsterisk('.mis1'));
-        $this->assertEquals('m2i*s', addAsterisk('m2is'));
-        $this->assertEquals('2n1s2', addAsterisk('2n1s2'));
-        $this->assertEquals('n2s*l', addAsterisk('n2sl'));
-        $this->assertEquals('s1l2', addAsterisk('s1l2'));
-        $this->assertEquals('s3l*a*t', addAsterisk('s3lat'));
-        $this->assertEquals('s*t4r', addAsterisk('st4r'));
-        $this->assertEquals('4t*e.', addAsterisk('4te.'));
-        $this->assertEquals('1t*r*a', addAsterisk('1tra'));
+        $this->assertEquals('m*i*s*t*r*a*n*s*l*a*t*e', addSymbol('mistranslate', '*'));
+        $this->assertEquals('.m*i*s1t*r*a*n*s*l*a*t*e.', addSymbol('.m*i*s1t*r*a*n*s*l*a*t*e.', '*'));
+        $this->assertEquals('a2n', addSymbol('a2n', '*'));
+        $this->assertEquals('.m*i*s1', addSymbol('.mis1', '*'));
+        $this->assertEquals('m2i*s', addSymbol('m2is', '*'));
+        $this->assertEquals('2n1s2', addSymbol('2n1s2', '*'));
+        $this->assertEquals('n2s*l', addSymbol('n2sl', '*'));
+        $this->assertEquals('s1l2', addSymbol('s1l2', '*'));
+        $this->assertEquals('s3l*a*t', addSymbol('s3lat', '*'));
+        $this->assertEquals('s*t4r', addSymbol('st4r', '*'));
+        $this->assertEquals('4t*e.', addSymbol('4te.', '*'));
+        $this->assertEquals('1t*r*a', addSymbol('1tra', '*'));
     }
 
     public function testNumberPosition()
@@ -36,12 +37,17 @@ class MainTest extends TestCase
         $this->assertEquals([1 => 1, 3 => 2], numberPosition('s1l2'));
         $this->assertEquals([1 => 3], numberPosition('s3lat'));
         $this->assertEquals([0 => 4], numberPosition('4te.'));
+        $this->assertEquals([6 => 1], numberPosition('.m*i*s1'));
     }
 
-    public function testHyphenate()
-    {
-        $this->assertEquals('m*i*s1t*r*a*n*s*l*a*t*e', hyphenate('mistranslate', [0 => '.mis1']));
-    }
+    // public function testHyphenate()
+    // {
+    //     require 'matches.txt';
+    //     $array = file('https://gist.githubusercontent.com/cosmologicon/1e7291714094d71a0e25678316141586/raw/006f7e9093dc7ad72b12ff9f1da649822e56d39d/tex-hyphenation-patterns.txt', FILE_IGNORE_NEW_LINES);
+
+    //     // $this->assertEquals('mis-trans-late', hyphenate('mistranslate', $array));
+    //     // $this->assertEquals('vig-or-ous', hyphenate('vigorous', [0 => '.mis1']));
+    // }
 
     // $start_array = [0 => ".mis1"];
     // $end_array = [0 => "4te."];
